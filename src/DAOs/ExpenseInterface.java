aa6c9f3cc395b9b28f41e5ebe643597b58da5a5f
Vialsiu <D00265675@student.dkit.ie>
2025-02-16 13:08:48 +0000

    Interfaces connected

--- src/DAOs/ExpenseInterface.java
+++ src/DAOs/ExpenseInterface.java
@@ -1,4 +1,13 @@
 package DAOs;
 
+import java.util.List;
+import java.util.Date;
+import DTOs.Expense;
+
 public interface ExpenseInterface {
+    void addExpense(Expense expense);
+    void deleteExpense(int expenseID);
+    List<Expense> listAllExpenses();
+    double calculateTotalExpenses();
+    List<Expense> listExpensesByMonth(int month, int year);
 }
--- src/DAOs/IncomeInterface.java
+++ src/DAOs/IncomeInterface.java
@@ -1,8 +1,13 @@
 package DAOs;
 
-
+import DTOs.Income;
 import java.util.List;
+import java.util.Date;
 
 public interface IncomeInterface {
-
-}
+     void addIncome(Income income);
+        void deleteIncome(int incomeID);
+        List<IncomeDao> listAllIncomes();
+        double calculateTotalIncome();
+        List<Income> listIncomesByMonth(int month, int year);
+    }
