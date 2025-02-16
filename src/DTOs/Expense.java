c03cecab3892050f66a05b7db2e3fdd38d2827ce
Vialsiu <D00265675@student.dkit.ie>
2025-02-16 11:58:19 +0000

    transferred my income and expense DAOs from a few commits ago but I also fixed them a bit to match my new sql

--- src/DTOs/Expense.java
+++ src/DTOs/Expense.java
@@ -1,4 +1,36 @@
 package DTOs;
 
+import java.sql.Date;
+
 public class Expense {
+    private int expenseID;
+    private String title;
+    private String category;
+    private double amount;
+    private Date dateIncurred;
+
+    public Expense(int expenseID, String title, String category, double amount, Date dateIncurred) {
+        this.expenseID = expenseID;
+        this.title = title;
+        this.category = category;
+        this.amount = amount;
+        this.dateIncurred = dateIncurred;
+    }
+
+    // ----- my constructor for inserts
+    public Expense(String title, String category, double amount, Date dateIncurred) {
+        this(0, title, category, amount, dateIncurred);
+    }
+
+    public int getExpenseID() { return expenseID; }
+    public String getTitle() { return title; }
+    public String getCategory() { return category; }
+    public double getAmount() { return amount; }
+    public Date getDateIncurred() { return dateIncurred; }
+
+    @Override
+    public String toString() {
+        return String.format("Expense [ID: %d, Title: %s, Category: %s, Amount: %.2f, Date: %s]",
+                expenseID, title, category, amount, dateIncurred);
+    }
 }
--- src/DTOs/Income.java
+++ src/DTOs/Income.java
@@ -1,4 +1,31 @@
 package DTOs;
 
+import java.sql.Date;
+
 public class Income {
+    private int incomeID;
+    private String title;
+    private String source;
+    private double amount;
+    private Date dateReceived;
+
+    public Income(int incomeID, String title, String source, double amount, Date dateReceived) {
+        this.incomeID = incomeID;
+        this.title = title;
+        this.source = source;
+        this.amount = amount;
+        this.dateReceived = dateReceived;
+    }
+
+    public int getIncomeID() { return incomeID; }
+    public String getTitle() { return title; }
+    public String getSource() { return source; }
+    public double getAmount() { return amount; }
+    public Date getDateReceived() { return dateReceived; }
+
+    @Override
+    public String toString() {
+        return String.format("Expense [ID: %d, Title: %s, Category: %s, Amount: %.2f, Date: %s]",
+                incomeID, title, source, amount, dateReceived);
+    }
 }
